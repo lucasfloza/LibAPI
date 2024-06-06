2877882ef4dcc6cfac7f34f5a6e28cd6d4a0e4f6
lucasfloza <lucasfsouza.l10@gmail.com>
2024-06-06 20:59:38 -0300

    Adding methods of getting all customers and updating customers

--- src/main/java/com/library/controller/v1/CustomerController.java
+++ src/main/java/com/library/controller/v1/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
@@ -22,10 +24,22 @@ public class CustomerController {
         return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 
+    @GetMapping("/")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> all = customerService.getAll();
+        return new ResponseEntity<>(all, HttpStatus.OK);
+    }
+
     @PostMapping("/create")
     public ResponseEntity<Customer> createCustomer(@RequestBody CustomerDto customerDto) {
         Customer customer = customerService.create(customerDto);
         return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{idCustomer}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long idCustomer, @RequestBody CustomerDto customerDto) {
+        Customer customer = customerService.update(idCustomer, customerDto);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{idCustomer}")
